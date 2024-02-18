c8cf7794d4578ae7bdc57e5a206c2c0566223ef5
ImNotAOwl <siegel.matthieu@ymail.com>
2024-02-18 22:41:30 +0100

    refacto: with md components

--- pages/about/index.vue
+++ pages/about/index.vue
@@ -2,16 +2,30 @@
 
 <template>
   <div>
-    <h1 class="text-5xl font-semibold text-center py-10">My experiences</h1>
+    <h1 class="text-5xl font-semibold text-center py-10">About</h1>
     <ContentQuery :path="`/_about/tv`" find="one" v-slot="{ data }">
-      <ContentRendererMarkdown :value="data" class="mb-10 text-pretty">
+      <!-- Bind with the <Tv/> component to render it -->
+
+      <ContentRendererMarkdown :value="data" class="text-pretty">
         <template #empty>
           <p>No content found.</p>
         </template>
       </ContentRendererMarkdown>
     </ContentQuery>
-    <HistoryExperience />
+    <ContentQuery :path="`/_about/it`" find="one" v-slot="{ data }">
+      <!-- Bind with the <Tv/> component to render it -->
+
+      <ContentRendererMarkdown :value="data" class="text-pretty">
+        <template #empty>
+          <p>No content found.</p>
+        </template>
+      </ContentRendererMarkdown>
+    </ContentQuery>
+
+
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+
+</style>
