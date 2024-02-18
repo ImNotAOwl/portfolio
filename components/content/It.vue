e6d93ba52d908b7670a78e2e10695198b7ad54ad
ImNotAOwl <siegel.matthieu@ymail.com>
2024-02-18 22:41:16 +0100

    component: for md binding

---
+++ components/content/It.vue
@@ -0,0 +1,54 @@
+<script setup lang="ts">
+interface Card {
+  logo: string;
+  job: string;
+  badge: Array<string>;
+  content: string;
+}
+
+const oldXP: Array<Card> = [
+  {
+    logo: "logos/logo_cfr_400x200.png",
+    job: "Developer in internship",
+    badge: ["PHP", "MySQL", "Database", "DevOps", "Deployment", "Git"],
+    content: "cfr",
+  },
+  {
+    logo: "logos/logo_oll_400x200.png",
+    job: "Developer Fullstack in apprenticeship",
+    badge: [
+      "Node.js",
+      "React.js",
+      "Python",
+      "DevOps",
+      "MariaDB",
+      "CI/CD",
+      "Git",
+      "Agile",
+    ],
+    content: "oll",
+  },
+  {
+    logo: "logos/logo_st_300x200.png",
+    job: "Developer Fullstack in apprenticeship",
+    badge: [
+      "Node.js",
+      "Vue.js",
+      "Python",
+      "DevOps",
+      "MongoDB",
+      "CI/CD",
+      "Git",
+      "Agile",
+    ],
+    content: "st",
+  },
+];
+</script>
+
+<template>
+	<h2 class="text-3xl font-semibold text-left py-5"> <slot name="title"/></h2>
+  <slot name="intro" class="my-10" />
+  <AboutExperience :card-list="oldXP" />
+  <slot name="after" />
+</template>
---
+++ components/content/Tv.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+interface Card {
+  logo: string;
+  job: string;
+  badge: Array<string>;
+  content: string;
+}
+
+const oldXP: Array<Card> = [
+  {
+    logo: "logos/logo_canal_400x200.png",
+    job: "Audiovisual Technician - CFC",
+    badge: ["TV", "Live", "Graphic Operator", "VizRT"],
+    content: "canal",
+  },
+  {
+    logo: "logos/logo_beinsports_400x200.png",
+    job: "Audiovisual Technician - Sports event",
+    badge: ["TV", "Live", "Graphic Operator", "VizRT"],
+    content: "beinsports",
+  },
+  {
+    logo: "logos/logo_tf1_400x200.png",
+    job: "Audiovisual Technician - News",
+    badge: ["TV", "Live", "Graphic Operator", "VizRT"],
+    content: "tf1",
+  },
+];
+</script>
+
+<template>
+	<h2 class="text-3xl font-semibold text-left py-5"> <slot name="title"/></h2>
+  <slot name="intro" class="my-10" />
+  <AboutExperience :card-list="oldXP" />
+  <slot name="after" />
+</template>
